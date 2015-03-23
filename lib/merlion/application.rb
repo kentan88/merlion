b5bbd43472fc80d214e86f85b0c74bd2159aa02f
Ken Tan <kentan0130@gmail.com>
2015-03-23 23:45:48 +0800

    looking goodg

--- lib/merlion/application.rb
+++ lib/merlion/application.rb
@@ -2,19 +2,29 @@ module Merlion
   class Application
     attr_reader :router
     def initialize
+      declare_global_variables
       handle_requires
-      @router = Merlion::Router.new
+      instantiate_router
+    end
+
+    def declare_global_variables
+      $AUTOLOAD_PATH = []
     end
 
     def handle_requires
-      $MERLION_LOAD_PATH = []
-      $MERLION_LOAD_PATH.unshift(Dir.pwd + "/app/models")
+      ["/app/config/initializers", "/app/models"].each do |dir|
+        $AUTOLOAD_PATH.unshift(Dir.pwd + dir)
+      end
 
-      $MERLION_LOAD_PATH.each do |load_path|
+      $AUTOLOAD_PATH.each do |load_path|
         Dir[load_path + "/**/*"].each do |file|
           require file if File.extname(file) == '.rb'
         end
       end
+    end
+
+    def instantiate_router
+      @router = Merlion::Router.new
     end
   end
 end
